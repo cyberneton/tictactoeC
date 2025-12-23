f578517cbc0136d5accee2dd9a3f3328ae79c208
Phongsakorn PP <128141477+pp300950@users.noreply.github.com>
2025-12-23 21:30:23 +0700

    add some table

--- play_ground.c
+++ play_ground.c
@@ -10,11 +10,16 @@ make table tictactoe in this file
  |   |   |    |
  ---+---+---+
 
-
 */
 #include <stdio.h>
 
 int main() {
+
+    for(int i = 0; i < 3; i++) {
+        printf(" ---+---+---+\n");
+        printf("|   |   |   |\n");
+    }
+    
     printf("I love eating fish, meowww\n");
     return 0;
 }
