188eabb32ecf2cad111965f0b51717d2d9cfef52
Cyberneton <na.serwisach@gmail.com>
2025-12-23 15:54:39 +0100

    Add tictactoe.c and update play_ground.c

--- play_ground.c
+++ play_ground.c
@@ -1,31 +1,24 @@
 /*
+ *
+ m *ake table tictactoe in this file
 
-make table tictactoe in this file
+    |   |
+ ---+---+---
+    |   |
+ ---+---+---
+    |   |
 
- ---+---+---+
-|   |   |    |
- ---+---+---+
- |   |   |    |
- ---+---+---+
- |   |   |    |
- ---+---+---+
-
-*/
+ */
 #include <stdio.h>
 
 int main() {
 
-    char[5]starer = "x"; // The first one that can be listed.
+    for(int i = 0; i < 2; i++) {
+        printf("    |   |    \n");
+        printf(" ---+---+--- \n");
 
-    for(int i = 0; i < 3; i++) {
-        printf("+---+---+---+\n");
-        printf("|   |   |   |\n");
     }
-
-    printf("+---+---+---+\n");
-
-    printf("\n input X or O:");
-
+    printf("    |   |    \n");
 
     printf("I love eating fish, meowww\n");
     return 0;
