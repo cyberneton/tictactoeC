3094b68095c4290fdfc31c11f582f9f12e5342f5
Phongsakorn PP <128141477+pp300950@users.noreply.github.com>
2025-12-23 21:43:27 +0700

    Add starter variable and prompt for player input in play_ground.c

--- play_ground.c
+++ play_ground.c
@@ -15,12 +15,17 @@ make table tictactoe in this file
 
 int main() {
 
+    char[5]starer = "x"; // The first one that can be listed.
+
     for(int i = 0; i < 3; i++) {
         printf("+---+---+---+\n");
         printf("|   |   |   |\n");
     }
 
     printf("+---+---+---+\n");
+
+    printf("\n input X or O:");
+
 
     printf("I love eating fish, meowww\n");
     return 0;
